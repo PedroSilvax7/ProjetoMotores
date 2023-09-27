f2a03ad87b525c61671453ddbd0831410a2ce893
Pedro Henrique do Nascimento Silva <20221170150043@ifrn.local>
2023-09-27 10:22:48 -0300

    Motores

--- My project/Assets/Scenes/Script/Player.cs
+++ My project/Assets/Scenes/Script/Player.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public int velocidade = 10;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start");
+        TryGetComponent(out rb);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         Debug.Log("Start");
+         float h = Input.GetAxis("Horizontal"); //-1 esquerda, 0 nada, 1 direita
+         float v = Input.GetAxis("Vertical"); // -1 pra tras, 0 nada, 1 pra cima 
+
+         Vector3 direcao = new Vector3(h, 0, v);
     }
 }
+
